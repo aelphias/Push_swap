be433da222cba7d6012de110d6c4410b29a6bf35
Alma Elphias <aelphias@am-b4.msk.21-school.ru>
2020-08-18 21:32:19 +0300

    strspit to allocate  **str

--- src/checker.c
+++ src/checker.c
@@ -6,7 +6,7 @@
 /*   By: aelphias <aelphias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/16 15:43:06 by aelphias          #+#    #+#             */
-/*   Updated: 2020/08/18 15:30:03 by aelphias         ###   ########.fr       */
+/*   Updated: 2020/08/18 21:27:42 by aelphias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,15 @@
 
 int	main(int argc, char **argv)
 {
+	char **str;
+	int i;
+
+	i = 0;
 	if (argc < 2)
 		exit(1);
 		/* atoi
 		isdigit
-		gnl 
+		gnl
 		if! int  || > int  || duplicates || instruction doesnt exis twrite(2, "Error\n", 6)
 		;*/
 	while (*argv[1])
@@ -26,5 +30,15 @@ int	main(int argc, char **argv)
 		ft_printf(" *argv[1]:%c", *argv[1]);
 		argv[1]++;
 	}
+	ft_printf("\n");
+	str = ft_strsplit(argv[1], ' ');
+	ft_printf("%s\n", str[0]);
+		
+		
+	ft_printf("hey\n");
+/* 	count = ft_atoi()
+ */
+
+	
 	return (0);
 }
