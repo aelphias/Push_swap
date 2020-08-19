87f41a1e19742fcf7115e9f829a8ae99bd17f2ce
Alma Elphias <aelphias@am-b4.msk.21-school.ru>
2020-08-20 00:56:17 +0300

    strsplit to work on

--- src/checker.c
+++ src/checker.c
@@ -6,7 +6,7 @@
 /*   By: aelphias <aelphias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/16 15:43:06 by aelphias          #+#    #+#             */
-/*   Updated: 2020/08/20 00:24:09 by aelphias         ###   ########.fr       */
+/*   Updated: 2020/08/20 00:51:46 by aelphias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	main(int argc, char **argv)
 {
 	char **str;
 	int i;
+	char		*stack_a;
 
 	i = 0;
 	if (argc < 2)
@@ -25,17 +26,25 @@ int	main(int argc, char **argv)
 		gnl
 		if! int  || > int  || duplicates || instruction doesnt exis twrite(2, "Error\n", 6)
 		;*/
+	ft_printf("argc:%d\n", argc);
 	while (argc > i)
 	{
 		str = ft_strsplit(argv[i], ' ');
+		while (*str)
+		{
+			ft_printf("{ 1 }\n");
+			stack_a = ft_strcat(stack_a,*str);
+			str++;
+		}
 		i++;
 	}
-	
-	while (*str != '\0' )
+/* 	while (*str != '\0' )
 	{
 		ft_printf("%s\n", *str);
 		str++;	
-	}
+	} */
+	ft_printf("stack_a: %s\n", stack_a);
+	
 	
 	ft_printf("\n I'm working, sunny!\n");
 	
